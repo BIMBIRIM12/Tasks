fb5d56c60c25c465a843b023a79f022fee4dac6f
BIMBIRIM12 <99718839+BIMBIRIM12@users.noreply.github.com>
2022-03-11 14:33:18 +0300

    Add files via upload

---
+++ Task2/CompareObjects/Objects.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+
+    /// <summary>
+    /// Создаёт динамический массив с типом значения Т.
+    /// </summary>
+    /// <typeparam name="T"> Тип значений массива </typeparam>
+    public class DynamicArray<T>
+    {
+        /// <summary>
+        /// Динамический массив с типом значений Т.
+        /// </summary>
+        public T[] myArray { get; set; }
+
+        /// <summary>
+        /// Емкость массива - внутренняя длина.
+        /// </summary>
+        private int Capacity => myArray.Count();
+
+        /// <summary>
+        /// Длина массива.
+        /// </summary>
+        private int Length => myArray.Length;
+
+        /// <summary>
+        /// Создаёт массив с определённой емкостью.
+        /// </summary>
+        /// <param name="size"> Размер массива </param>
+        /// <exception cref="ArgumentNullException"> Проверка длины массива на коррекстность </exception>
+        public DynamicArray(int size)
+        {
+            if(size <= null)
+            {
+                throw new ArgumentNullException(nameof(size), "Size is null");
+            }
+            else
+            {
+                myArray = new T[size];
+            }
+        }
+    }
+}
---
+++ Task2/CompareObjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class CompareObjects
+    {
+        static void Main()
+        {
+            ComparisObjects();
+        }
+
+        public static void ComparisObjects()
+        {
+            var objectFirst = new DynamicArray<int>(5);
+            var objectSecond = new DynamicArray<string>(6);
+            var objectThree = GetObjectThree();
+            var objectFour = GetObjectFour();
+
+            objectFirst.Equals(objectSecond);
+            objectFirst.Equals(objectThree);
+            objectSecond.Equals(objectThree);
+            objectThree.Equals(objectFour); 
+        }
+
+        private static int[] GetObjectThree()
+        {
+            int[] objectThree = new int[5] { 1, 2, 3, 4, 5 };
+            return objectThree;
+        }
+
+        private static double[] GetObjectFour()
+        {
+            double[] objectFour = new double[6] { 1.2, 12, 321, 2.2, 1221.2, 333.1 };
+            return objectFour;
+        }
+    }
+}

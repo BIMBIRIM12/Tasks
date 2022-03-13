6a5fc471568353914312fad832780dca800d3e55
BIMBIRIM12 <99718839+BIMBIRIM12@users.noreply.github.com>
2022-03-13 17:32:09 +0300

    Add files via upload

---
+++ Task3/ExampleDelegatesAndEvents/ExampleDelegateAndEvent.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Program
+{
+    #region Delegates
+    public delegate void CompleteComparesMessage(string message);
+
+    public delegate bool CompareNumberAndTen(int number);
+
+    public delegate bool CompareNumberAndOneHundred(int number);
+
+    public delegate bool ComparingNumbersAndOneThousand(int number);
+    #endregion
+
+    public class ExampleDelegateAndEvent
+    {
+        /// <summary>
+        /// Представляет сравнение введённого пользователем числа с 10, 100 и 1000 и возвращает результат.
+        /// </summary>
+        #region Events
+        public event CompleteComparesMessage? ComparedCompleted;
+
+        public event ComparingNumbersAndOneThousand? ComparingWithOneThousand;
+        #endregion
+
+        /// <summary>
+        /// Принимает число от пользователя и передаёт для сравнения в методы.
+        /// </summary>
+        /// <param name="number"> число от пользователя </param>
+        public void CompareNumbers(int number)
+        {
+            CompareNumberAndTen compareTen = new(CompareNumbersAndTen);
+            var value = compareTen(number);
+            Console.WriteLine($"{number} > 10 = {value}");
+
+            CompareNumberAndOneHundred(number);
+
+            ComparingWithOneThousand += CompareNumbersAndOneThousand;
+            Console.WriteLine($"{number} > 1000 = {ComparingWithOneThousand(number)}");
+            ComparedCompleted?.Invoke("Compare complete");
+        }
+        #region Compare
+
+        /// <summary>
+        /// Принимает число через делегат и сравнивает его с 10.
+        /// </summary>
+        /// <param name="number"> Число от пользователя </param>
+        /// <returns> Результат сравнения </returns>
+        public bool CompareNumbersAndTen(int number)
+        {
+            if (number > 10)
+            {               
+                return true; 
+            }
+            else
+            {
+                return false;              
+            }
+        }
+
+        /// <summary>
+        /// Принимает число через событие и сравнивает его с 1000.
+        /// </summary>
+        /// <param name="number"> Число от пользователя </param>
+        /// <returns> Резуьтат сравнения </returns>
+        public bool CompareNumbersAndOneThousand(int number)
+        { 
+            if (number > 1000)
+            {               
+                return true;
+            }
+            else
+            {               
+                return false;
+            }           
+        }
+
+        /// <summary>
+        /// Сравнивает число от пользователя с 1000 через лямбду-выражение.
+        /// </summary>
+        /// <param name="number"> Число от пользователя </param>
+        /// <returns> Результат сравнения </returns>
+        public bool CompareNumberAndOneHundred(int number)
+        {
+            CompareNumberAndOneHundred compareNumberAndOneHundred = (number) => number > 100;
+            bool result = compareNumberAndOneHundred(number);
+            Console.WriteLine($"{number} > 100 = {result}");
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ Task3/ExampleDelegatesAndEvents/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Program
+{
+    public class DelegateAndEvents
+    { 
+        static void Main()
+        {
+            DemoExample();
+        }
+
+        public static void DemoExample()
+        {
+            var numberFromUser = ToInt(GetStringFromUser());
+            var example = new ExampleDelegateAndEvent();
+            void DisplayMessage(string message) => Console.WriteLine(message);
+            example.ComparedCompleted += DisplayMessage;
+            example.CompareNumbers(numberFromUser);
+        }
+        #region GetNumberFromUser
+        public static int ToInt(string stringFromUser)
+        {
+            if (String.IsNullOrEmpty(stringFromUser))
+            {
+                throw new ArgumentNullException(nameof(stringFromUser), "String is empty");
+            }
+            else
+            {
+                return int.Parse(stringFromUser);
+            }     
+        }
+
+        private static string GetStringFromUser()
+        {
+            Console.WriteLine("Enter number");
+            return Console.ReadLine();
+        }
+        #endregion
+    }
+}

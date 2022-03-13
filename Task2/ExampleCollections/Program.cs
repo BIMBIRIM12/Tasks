f9155981faaa7067e0b7edb2179ff268aa656e4e
BIMBIRIM12 <99718839+BIMBIRIM12@users.noreply.github.com>
2022-03-13 17:48:12 +0300

    Add files via upload

--- Task2/ExampleCollections/Program.cs
+++ Task2/ExampleCollections/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace Program
 {
+
+    /// <summary>
+    /// Представляет примеры работ с разными видами коллекций.
+    /// </summary>
     public class ExampleCollections
     {
         static void Main()
@@ -13,6 +17,10 @@ namespace Program
             var queue = GetExampleQueue(list);
         }
 
+        /// <summary>
+        /// Представляет пример создания, добавление, удаления, сортировки элементов списка.
+        /// </summary>
+        /// <returns> Список с названиями автомобилей </returns>
         public static List<string> GetExampleList()
         {
             var carsBrand = new List<string>();
@@ -25,53 +33,88 @@ namespace Program
             return carsBrand;
         }
 
+        /// <summary>
+        /// Представляет пример создания, заполнения, добавления и удаления элементов словаря. 
+        /// </summary>
+        /// <param name="list"> Список с марками автомобилей </param>
+        /// <returns> Словарь, содержащий список марок автомобилей и частоту их встречаемости </returns>
+        /// <exception cref="ArgumentNullException"> Проверка входящего списка на наличие элементов </exception>
         public static Dictionary<string, int> GetExampleDictionary(List<string> list)
         {
-            var cars = new Dictionary<string, int>();
-            for(var i = 0; i < list.Count; i++)
+            if (list == null)
             {
-                if (cars.ContainsKey(list[i]))
+                throw new ArgumentNullException(nameof(list), "List is null");
+            }
+            else
+            {
+                var cars = new Dictionary<string, int>();
+                for (var i = 0; i < list.Count; i++)
                 {
-                    cars[list[i]]++;
+                    if (cars.ContainsKey(list[i]))
+                    {
+                        cars[list[i]]++;
+                    }
+                    else
+                    {
+                        cars.Add(list[i], 1);
+                    }
                 }
-                else
-                {
-                    cars.Add(list[i], 1);
-                } 
+
+                cars.Add("Reanault", 1);
+                cars.Remove("Renault");
+                return cars;
             }
 
-            cars.Add("Reanault", 1);
-            cars.Remove("Renault");
-            return cars;
         }
 
+        /// <summary>
+        /// Представляет пример создания, заполнения и удаления элементов стэка.
+        /// </summary>
+        /// <param name="list"> Список с марками автомобилей </param>
+        /// <returns> Стэк с марками автомобилей </returns>
+        /// <exception cref="ArgumentNullException"> Проверка входящего списка на наличие элементов </exception>
         public static Stack<string> GetExampleStack(List<string> list)
         {
-            var stackCars = new Stack<string>();
-            for(var i = 0; i < list.Count; i++)
+            if (list == null)
             {
-                stackCars.Push(list[i]);
+                throw new ArgumentNullException(nameof(list), "List is null");
             }
+            else
+            {
+                var stackCars = new Stack<string>();
+                for (var i = 0; i < list.Count; i++)
+                {
+                    stackCars.Push(list[i]);
+                }
 
-            stackCars.Pop();
-            return stackCars;
+                stackCars.Pop();
+                return stackCars;
+            }
         }
 
+        /// <summary>
+        /// Представляет пример создания, заполнения, добавления и удаления элементов очереди.
+        /// </summary>
+        /// <param name="list"> Список с марками автомобилей </param>
+        /// <returns> Очередь с марками автомобилей </returns>
+        /// <exception cref="ArgumentNullException"> Проверка входящего списка на наличие значений </exception>
         public static Queue<string> GetExampleQueue (List<string> list)
         {
-            var queueCars = new Queue<string>();
-            for (var i = 0;i < list.Count; i++)
+            if (list == null)
             {
-                queueCars.Enqueue(list[i]);
+                throw new ArgumentNullException(nameof(list), "List is null");
             }
-
-            queueCars.Dequeue();
-            foreach(var car in queueCars)
+            else
             {
-                Console.WriteLine(car);
-            }
+                var queueCars = new Queue<string>();
+                for (var i = 0; i < list.Count; i++)
+                {
+                    queueCars.Enqueue(list[i]);
+                }
 
-            return queueCars;
+                queueCars.Dequeue();
+                return queueCars;
+            }
         }
     }
 }

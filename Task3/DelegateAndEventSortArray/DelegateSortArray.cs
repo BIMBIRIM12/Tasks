cee40215d53dc5bb1edd2dcbf92ee84e16775833
BIMBIRIM12 <99718839+BIMBIRIM12@users.noreply.github.com>
2022-03-13 15:20:39 +0300

    Add files via upload

---
+++ Task3/DelegateAndEventSortArray/DelegateSortArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public delegate int[] GetFinishSortingArray(int[] array);
+
+    public delegate void StringMessage(string message);
+
+    class DelegateSortArray
+    {
+        public event StringMessage? CompletedSorting;
+   
+        public int[] GetSortArray()
+        {
+            int[] arrayUnsort = new int[5] { 1, 43, 5, 90, 2 };
+            GetFinishSortingArray sortArray = new(SortArray);
+            int[] value = sortArray(arrayUnsort);
+
+            return value;
+        }
+
+        public int[] SortArray(int[] arrayUnsort)
+        {
+            #region Sorting
+            int[] arraySort = arrayUnsort;
+            for (var i = 0; i < arraySort.Length - 1; i++)
+            {
+                if (arraySort[i] > arraySort[i + 1])
+                {
+                    for (var j = i + 1; j > 0; j--)
+                    {
+                        if (arraySort[j] < arraySort[j - 1])
+                        {
+                            var g = arraySort[j];
+                            arraySort[j] = arraySort[j - 1];
+                            arraySort[j - 1] = g;
+                        }
+                    }
+                }
+            }
+            #endregion
+            CompletedSorting?.Invoke("Sorting completed");
+            foreach (var number in arraySort)
+            {
+                Console.WriteLine(number);
+            }
+
+            return arraySort;
+        }
+    }
+}   
+
---
+++ Task3/DelegateAndEventSortArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program
+{
+    class DelegateAndEvents
+    {
+        static void Main()
+        {
+            MoveDelegateAndEvent();
+        }
+
+        private static void MoveDelegateAndEvent()
+        {
+            var sortingArray = new DelegateSortArray();
+            void DisplayMessage(string message) => Console.WriteLine(message);
+            sortingArray.CompletedSorting += DisplayMessage;
+            int[] sortiArray = sortingArray.GetSortArray();          
+        }
+    }
+}

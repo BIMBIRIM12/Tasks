efb4c852252ad9b81ee178880044ac8971a75763
BIMBIRIM12 <99718839+BIMBIRIM12@users.noreply.github.com>
2022-03-23 15:23:31 +0300

    Add files via upload

--- Task4/Json/Program.cs
+++ Task4/Json/Program.cs
@@ -5,13 +5,19 @@ using System.Configuration;
 
 namespace Program
 {
-    public class XmlAndJason
-    {     
+    /// <summary>
+    /// Сереализирует и десеализирует объект в JSON.
+    /// </summary>
+    public class Json
+    {   
+        /// <summary>
+        /// Сереализирует объект Person в JSON и десереализирует его.
+        /// </summary>       
         static void Main(string[] args)
         {
             var currentConfig = ConfigurationManager.AppSettings.Get("pathJsonDate");
    
-            Person anton = new Person("Anton", "Nozdrin", 26);  
+            Person anton = new Person("Anton", "Nozdrin", 26, "man");
             var jsonFormatter = new DataContractJsonSerializer(typeof(Person));
 
             using (var file = new FileStream("person.json", FileMode.OpenOrCreate))
@@ -26,6 +32,9 @@ namespace Program
         }       
     }
 
+    /// <summary>
+    /// Создаёт объект Person с 3 параметрами - Name, Surname и Age.
+    /// </summary>
     [DataContract]
     class Person
     {
@@ -39,14 +48,32 @@ namespace Program
         [DataMember]
         public int Age { get; set; }
 
-        public Person (string name, string surname, int age)
+        [DataMember]
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// Присвавивает полям значения.
+        /// </summary>
+        /// <param name="name"> Имя </param>
+        /// <param name="surname"> Фамилия </param>
+        /// <param name="age"> Возраст </param>
+        public Person (string name, string surname, int age, string gender)
         {
             CheckingParameters(name, surname, age);
             Name = name;
             Surname = surname;
             Age = age;
+            Gender = gender;
         }
        
+        /// <summary>
+        /// Проверяет входящие значение
+        /// </summary>
+        /// <param name="name"> Имя </param>
+        /// <param name="surname"> Фамилия </param>
+        /// <param name="age"> Возраст </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public static void CheckingParameters(string name, string surname, int age)
         {
             if(name == null)

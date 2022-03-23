379c6116094cf231de9ef7af47be667c65d9d070
BIMBIRIM12 <99718839+BIMBIRIM12@users.noreply.github.com>
2022-03-23 13:45:31 +0300

    Add files via upload

---
+++ Task4/Json/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.Json;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace Program
+{
+    public class XmlAndJason
+    {     
+        static void Main(string[] args)
+        {
+            Person anton = new Person("Anton", "Nozdrin", 26);  
+            var jsonFormatter = new DataContractJsonSerializer(typeof(Person));
+
+            using (var file = new FileStream("person.json", FileMode.OpenOrCreate))
+            {
+               jsonFormatter.WriteObject(file, anton);
+            }
+
+            using (var file = new FileStream("person.json", FileMode.OpenOrCreate))
+            {
+               var newPerson = jsonFormatter.ReadObject(file) as Person;
+            }
+        }       
+    }
+
+    [DataContract]
+    class Person
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public int Age { get; set; }
+
+        public Person (string name, string surname, int age)
+        {
+            CheckingParameters(name, surname, age);
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+       
+        public static void CheckingParameters(string name, string surname, int age)
+        {
+            if(name == null)
+            {
+                throw new ArgumentNullException(nameof(name), "Name is empty");
+            }
+
+            if(surname == null)
+            {
+                throw new ArgumentNullException(nameof(surname), "Surname is empty");
+            }
+
+            if(age <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(age), "Age equals null or less than null");
+            }
+        }
+    }
+}

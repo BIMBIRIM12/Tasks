200b431dc862956a7654b767ba4c41787f30e53b
BIMBIRIM12 <99718839+BIMBIRIM12@users.noreply.github.com>
2022-03-24 17:57:31 +0300

    Add files via upload

---
+++ Task4/Json/Person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Configuration;
+
+namespace Program
+{
+   [DataContract]
+    internal class Person
+    {
+        /// <summary>
+        /// Имя человека.
+        /// </summary>
+        [DataMember]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Фамилия человека
+        /// </summary>
+        [DataMember]
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Возраст человека
+        /// </summary>
+        [DataMember]
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Пол человека
+        /// </summary>
+        [DataMember]
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// Присвавивает полям значения.
+        /// </summary>
+        /// <param name="name"> Имя </param>
+        /// <param name="surname"> Фамилия </param>
+        /// <param name="age"> Возраст </param>
+        public Person(string name, string surname, int age, string gender)
+        {
+            CheckParameters(name, surname, age, gender);
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Gender = gender;
+        }
+
+        /// <summary>
+        /// Проверяет входящие значение
+        /// </summary>
+        /// <param name="name"> Имя </param>
+        /// <param name="surname"> Фамилия </param>
+        /// <param name="age"> Возраст </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static void CheckParameters(string name, string surname, int age, string gender)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name), "Name is empty");
+            }
+
+            if (string.IsNullOrEmpty(surname))
+            {
+                throw new ArgumentNullException(nameof(surname), "Surname is empty");
+            }
+
+            if ( age <= 0 | age> 90 )
+            {
+                throw new ArgumentOutOfRangeException(nameof(age), "Age is incorrect");
+            }
+
+            if (string.IsNullOrEmpty(gender))
+            {
+                throw new ArgumentNullException(nameof(gender), "Gender is empty");
+            }
+        }
+    }
+}
+
--- Task4/Json/Program.cs
+++ Task4/Json/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Configuration;
 
@@ -10,91 +9,44 @@ namespace Program
     /// </summary>
     public class Json
     {   
-        /// <summary>
-        /// Сереализирует объект Person в JSON и десереализирует его.
-        /// </summary>       
         static void Main(string[] args)
         {
-            var currentConfig = ConfigurationManager.AppSettings.Get("pathJsonDate");
-   
-            Person anton = new Person("Anton", "Nozdrin", 26, "man");
+            ConvertJson();     
+        }    
+
+        /// <summary>
+        /// Сереализирует объект Person в JSON, затем десереализирует его.
+        /// </summary>
+        private static void ConvertJson()
+        {
+            var currentConfig = ConfigurationManager.AppSettings["pathJsonDate"];
+
+            var anton = new Person("Anton", "Kunin", 28, "man");
             var jsonFormatter = new DataContractJsonSerializer(typeof(Person));
 
-            using (var file = new FileStream("person.json", FileMode.OpenOrCreate))
+            using (var file = new FileStream(currentConfig, FileMode.OpenOrCreate))
             {
-               jsonFormatter.WriteObject(file, anton);
+                 jsonFormatter.WriteObject(file, anton);
             }
 
-            using (var file = new FileStream("person.json", FileMode.OpenOrCreate))
+            using (var loadedData = new FileStream(currentConfig, FileMode.OpenOrCreate))
             {
-               var newPerson = jsonFormatter.ReadObject(file) as Person;
-            }
-        }       
-    }
-
-    /// <summary>
-    /// Создаёт объект Person с 3 параметрами - Name, Surname и Age.
-    /// </summary>
-    [DataContract]
-    class Person
-    {
-        [DataMember]
-        public string Name { get; set; }
-
-
-        [DataMember]
-        public string Surname { get; set; }
-
-        [DataMember]
-        public int Age { get; set; }
-
-        [DataMember]
-        public string Gender { get; set; }
-
-        /// <summary>
-        /// Присвавивает полям значения.
-        /// </summary>
-        /// <param name="name"> Имя </param>
-        /// <param name="surname"> Фамилия </param>
-        /// <param name="age"> Возраст </param>
-        public Person (string name, string surname, int age, string gender)
-        {
-            CheckingParameters(name, surname, age, gender);
-            Name = name;
-            Surname = surname;
-            Age = age;
-            Gender = gender;
-        }
-       
-        /// <summary>
-        /// Проверяет входящие значение
-        /// </summary>
-        /// <param name="name"> Имя </param>
-        /// <param name="surname"> Фамилия </param>
-        /// <param name="age"> Возраст </param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public static void CheckingParameters(string name, string surname, int age, string gender)
-        {
-            if(name == null)
-            {
-                throw new ArgumentNullException(nameof(name), "Name is empty");
-            }
-
-            if(surname == null)
-            {
-                throw new ArgumentNullException(nameof(surname), "Surname is empty");
-            }
-
-            if(age <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(age), "Age equals null or less than null");
-            }
-
-            if(gender == null)
-            {
-                throw new ArgumentNullException(nameof(gender), "Gender is empty");
+                var newPerson = jsonFormatter.ReadObject(loadedData) as Person;
+                ShowNewPerson(newPerson);
             }
         }
-    }
+
+        /// <summary>
+        /// Выводит значения всех свойств десерелизируемого объекта в консоль.
+        /// </summary>
+        /// <param name="newPerson"></param>
+        private static void ShowNewPerson(Person newPerson)
+        {
+            var  properties = newPerson.GetType().GetProperties();
+            for(var i = 0; i < properties.Length; i++)
+            {
+                Console.WriteLine(properties[i].GetValue(newPerson));
+            }   
+        }
+    } 
 }

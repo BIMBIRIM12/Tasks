aab2facd12897df225688abe4ad2e7a7036db841
BIMBIRIM12 <99718839+BIMBIRIM12@users.noreply.github.com>
2022-04-03 12:14:01 +0300

    Add files via upload

---
+++ Task5/Linq/Person.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQExample
+{
+    /// <summary>
+    /// Примеры работы с Linq.
+    /// </summary>
+    public class Person
+    {
+        #region Properties
+        // Имя человека.
+        private string Name { get; set; }
+
+        // Фамилия человека.
+        private string Surname { get; set; }
+
+        // Возраст человека.
+        private int Age { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Примеры работы с Linq с коллекцией.
+        /// </summary>
+        public static void GetListPerson()
+        {
+            // Массив, содержащий в себе 4 объекта типа Person.
+            var persons = new Person[]
+            {
+                new Person {Name = "Artem", Surname = "Shibanov", Age = 21},
+                new Person {Name = "Roman", Surname = "Nozdrin", Age = 21},
+                new Person {Name = "Gleb", Surname = "Rodionov", Age = 19}, 
+                new Person {Name = "Artem", Surname = "Koreskov", Age = 25}
+            };
+            #region LinqOperations
+            // Доступ к массиву через фильтрацию.
+            var selectedPersonAge = persons.Where(n => n.Age == 21);
+            var selectedPersonName = from n in persons
+                                     where n.Name == "Artem"
+                                     select n;
+            // Анонимный тип.
+            var nikita = new[]
+            {
+                new {Name = "Nikita", Surname = "Kibardin", Age = 21},
+                new {Name = "Oleg", Surname = "Kirieshkin", Age = 24}
+            };
+
+            // Доступ к массиву через группироку.
+            var groupedPersonAge = persons.GroupBy(n => n.Age > 20);
+            var groupedPersonName = from n in persons
+                                    group n by (n.Name == "Artem") into groups
+                                    select groups;
+
+            // Упорядочивание массива по имени человека.
+            var orderByName = persons.OrderBy(n => n.Name);          
+            var orderByAge = from n in persons
+                              orderby n.Age
+                              select n;
+
+            //Массив в обратном порядке.
+            var reversePersons = from n in persons.Reverse()
+                    select n;
+            var reversePerson = persons.Reverse();
+            #endregion
+
+
+
+        }
+    }
+}
---
+++ Task5/Linq/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinQExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            GetListInt();
+            Person.GetListPerson();
+        }
+
+        /// <summary>
+        /// Создаётся список целых чисел от 1 до 10 и демонстрируются некоторые Linq операции для работы с ним.
+        /// </summary>
+        public static void GetListInt()
+        {
+            var numbers = new List<int>();
+            for(var i = 1; i < 11; i++)
+            {
+                numbers.Add(i);
+            }
+            #region LinqOperations
+            var castListIntToStringArary = numbers.Cast<int>();// Преобразует список чисел в простой массив чисел.
+            var listToArray = numbers.ToArray(); // Преобразование списка int значений в массив.         
+            var arrayToList = listToArray.ToList<int>(); // Преобразование массива в список int значений.
+            var listTake8 = numbers.Take(8); // Взять только первые 8 элементов списка.
+            var listSkip5 = numbers.Skip(5); // Пропустить первые 5 элементов списка.
+            var listOrderBy = numbers.OrderByDescending(n => n); // Сортировка списка по убыванию.
+            var listAny = numbers.Any(n => n==5); // Проверка на наличие элемента 5 в списке.
+            var lisAll = numbers.All(n => n == 4); // Проверка на то, все ли элементы списка это 4.
+            var firstElement = numbers.First(); // Первый элемент списка.
+            var firstElementMultipleThree = numbers.First(n => n % 3 == 0); // Первый элемент списка, кратный 3.
+            var agregatedNumbers = numbers.Aggregate((a, b) => (a * b));// Произведение всех элементов списка.
+            #endregion
+
+
+        }
+    }
+}
